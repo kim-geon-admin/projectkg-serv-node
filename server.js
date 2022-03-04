ee65937bbde698a6bc49277c60473f122eda7f1d
kg <nayaguny@gmail.com>
2022-03-04 23:30:15 +0900

    20220304

--- server.js
+++ server.js
@@ -1,23 +1,31 @@
 const express = require('express');
 const app = express();
-const connection = require('./mysql/connection');
-const executeQueryBoard = require('./mysql/executeQueryBoard');
 
+const cors = require('cors');
+const middleRouter = require('./router/middleRouter');
+const bodyParser = require('body-parser');
 
-app.listen(3000,function(){
-    console.log('listning 3000');
+app.listen(8080,function(){
+    console.log('listning 8080');
 
 }); 
-
+app.use(cors()); 
+app.use(bodyParser.json());
 //app.use(express.static('public'));
 
-app.get('/',function(request,response){
-    response.send('home 입니다 2');
+app.use('/api',middleRouter);
+/*
+app.get('/api',function(request,response){
+   // response.send('home 입니다 2');
+  // response.header("Access-Control-Allow-Origin","*");
     executeQueryBoard.selectBoard(response);
 });
+*/
 
+/*
 app.get('/login',function(request,response){
     response.send('login 창 입니다'+request.query.id);
     //console.log(__dirname);
     //response.sendFile(__dirname+'/index.html');
 });
+*/
